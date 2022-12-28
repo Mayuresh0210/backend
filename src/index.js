a20adce581bd3d95ddbc1e22d3a27ea6764076bd
Mayuresh0210 <121124304+Mayuresh0210@users.noreply.github.com>
2022-12-28 18:59:11 +0530

    backend is ready with basic functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import  express  from "express";
 import bluebird from "bluebird";
 import {createConnection} from "mysql";
+import cors from "cors";
 
 const connectionm ={
     host : "localhost",
@@ -12,6 +13,7 @@ const connectionm ={
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
+app.use(cors());
 
 /* http://localhost:3002/ */
 app.get("/", (req,res) => res.send("hello kasturi"));
